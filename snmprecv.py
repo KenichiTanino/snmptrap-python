49a2e93ac7c3e5fd9347c8b088d2683012d32720
Kenichi Tanino <ke.tanino@gmail.com>
2023-10-22 11:24:02 +0900

    fix: add uuid and async cmd

--- snmprecv.py
+++ snmprecv.py
@@ -1,11 +1,16 @@
+import asyncio
+import pickle
+
 from loguru import logger
 from pysnmp.entity import engine, config
 from pysnmp.carrier.asyncore.dgram import udp, udp6
 from pysnmp.entity.rfc3413 import ntfrcv
+import uuid6
+
 
 logger.add("app.log", format="{level} : {time} : {message}: {process}")
 
-bindIF = "172.22.123.3"
+bindIF = "0.0.0.0"
 
 
 def requestObserver(snmpEngine, execpoint, variables, cbCtx):
@@ -25,9 +30,38 @@ def cbFun(snmpEngine, stateReference, contextEngineId, contextName,
           varBinds, cbCtx):
     logger.info('Notify from ContextId "%s", ContextName "%s"' % (contextEngineId.prettyPrint(),
                                                             contextName.prettyPrint()))
+    uuid = uuid6.uuid7()
+    filename = f"{uuid}-snmptrap.pickle"
+    try:
+        # JSON形式でファイルに出力
+        with open(filename, 'wb') as f:
+            pickle.dump(varBinds, f, protocol=5)
 
-    for name, val in varBinds:
-        logger.info('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
+    except TypeError as e:
+        # JSON形式で保存できない場合はpickleを使用
+        logger.exception(e)
+
+    # for name, val in varBinds:
+    #    logger.info('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
+
+    # 対応するコマンドを起動する
+    cmd = f"/bin/ls {filename}"
+    asyncio.run(run(cmd))
+
+
+async def run(cmd):
+    proc = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+
+    stdout, stderr = await proc.communicate()
+
+    logger.info(f'[{cmd!r} exited with {proc.returncode}]')
+    if stdout:
+        logger.info(f"{stdout.decode()}")
+    if stderr:
+        logger.error(f"{stderr.decode()}")
 
 
 def main():
